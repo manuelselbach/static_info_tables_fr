48ab2274c3df02721c95eddd7b6263585cf44923
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-06-30 11:35:56 +0000

    Added French names for Switzerland country zones Released to TER as version 2.0.2
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/static_info_tables_fr/trunk@21922 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "static_info_tables_fr"
 #
-# Auto generated 10-06-2009 14:10
+# Auto generated 30-06-2009 13:35
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -15,8 +15,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => '(fr) language pack for the Static Info Tables providing localized names for countries, currencies and so on.',
 	'category' => 'misc',
 	'shy' => 0,
-	'version' => '2.0.1',
-	'dependencies' => '',
+	'version' => '2.0.2',
+	'dependencies' => 'static_info_tables',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
@@ -43,7 +43,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:9:{s:9:"ChangeLog";s:4:"2687";s:20:"class.ext_update.php";s:4:"8c78";s:21:"ext_conf_template.txt";s:4:"7de5";s:12:"ext_icon.gif";s:4:"639f";s:17:"ext_localconf.php";s:4:"48db";s:14:"ext_tables.php";s:4:"2c0d";s:14:"ext_tables.sql";s:4:"c1d1";s:28:"ext_tables_static_update.sql";s:4:"c4fe";s:16:"locallang_db.xml";s:4:"213e";}',
+	'_md5_values_when_last_written' => 'a:9:{s:9:"ChangeLog";s:4:"fd0a";s:20:"class.ext_update.php";s:4:"ac43";s:21:"ext_conf_template.txt";s:4:"7de5";s:12:"ext_icon.gif";s:4:"639f";s:17:"ext_localconf.php";s:4:"48db";s:14:"ext_tables.php";s:4:"15d9";s:14:"ext_tables.sql";s:4:"6b82";s:28:"ext_tables_static_update.sql";s:4:"953a";s:16:"locallang_db.xml";s:4:"213e";}',
+	'suggests' => array(
+	),
 );
 
 ?>
