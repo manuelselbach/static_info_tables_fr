e56cc2c891eb689d2a4a7f6f3abadb1a5f0f2684
Francois Suter <francois@typo3.org>
2015-11-17 17:26:27 +0100

    [BUGFIX] TCA cleanup
    Removed obsolete properties and cleaned up the TCA extension
    to match 6.2+ standards.
    
    Resolves: #71350
    Releases: 6.1
    Change-Id: I4fbce23d23b3108731d8e28236ca2ce5628f2029
    Reviewed-on: https://review.typo3.org/44757
    Reviewed-by: Francois Suter <francois@typo3.org>
    Tested-by: Francois Suter <francois@typo3.org>

---
+++ Configuration/TCA/Overrides/static_countries.php
@@ -0,0 +1,16 @@
+<?php
+defined('TYPO3_MODE') or die();
+
+$sourceField = 'cn_short_en';
+$destField = 'cn_short_fr';
+
+$additionalColumns = array();
+$additionalColumns[$destField] = $GLOBALS['TCA']['static_countries']['columns'][$sourceField];
+$additionalColumns[$destField]['label'] = 'LLL:EXT:static_info_tables_fr/Resources/Private/Language/locallang_db.xlf:static_countries_item.' . $destField;
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('static_countries', $additionalColumns);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('static_countries', $destField, '', 'after:' . $sourceField);
+// Add as search field
+$GLOBALS['TCA']['static_countries']['ctrl']['searchFields'] .= ',' . $destField;
+
+unset($additionalColumns, $additionalFields);
---
+++ Configuration/TCA/Overrides/static_languages.php
@@ -0,0 +1,16 @@
+<?php
+defined('TYPO3_MODE') or die();
+
+$sourceField = 'lg_name_en';
+$destField = 'lg_name_fr';
+
+$additionalColumns = array();
+$additionalColumns[$destField] = $GLOBALS['TCA']['static_languages']['columns'][$sourceField];
+$additionalColumns[$destField]['label'] = 'LLL:EXT:static_info_tables_fr/Resources/Private/Language/locallang_db.xlf:static_languages_item.' . $destField;
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('static_languages', $additionalColumns);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('static_languages', $destField, '', 'after:' . $sourceField);
+// Add as search field
+$GLOBALS['TCA']['static_languages']['ctrl']['searchFields'] .= ',' . $destField;
+
+unset($additionalColumns, $additionalFields);
