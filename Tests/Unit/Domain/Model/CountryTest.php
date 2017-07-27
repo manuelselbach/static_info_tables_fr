02b1eca9a7705be3d2276cf2caf4977a603b4821
Manuel Selbach <manuel_selbach@yahoo.de>
2017-07-27 20:27:17 +0200

    Add dummy test

---
+++ Tests/Unit/Domain/Model/CountryTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace Mselbach\StaticInfoTablesUa\Tests\Unit\Domain\Model;
+use TYPO3\CMS\Core\Tests\UnitTestCase;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+/**
+ * CountryTest
+ */
+class CountryTest extends UnitTestCase
+{
+    /**
+     * @test
+     */
+    public function testDummy()
+    {
+    }
+}
