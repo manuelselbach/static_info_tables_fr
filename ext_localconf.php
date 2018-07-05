4de451a5c81384670093079effa0c5c772532b31
Manuel Selbach <manuel_selbach@yahoo.de>
2018-07-05 21:42:23 +0200

    [#2] Update compatibility to static_info_tables 6.5

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,6 +5,9 @@ $initialize = function ($extKey) {
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
         '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extKey . '/Configuration/TypoScript/Extbase/setup.txt">'
     );
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extKey . '/Configuration/PageTSconfig/Suggest.tsconfig">'
+    );
 };
 $initialize(\Mselbach\StaticInfoTablesFr\Extension::EXTENSION_KEY);
 unset($initialize);
