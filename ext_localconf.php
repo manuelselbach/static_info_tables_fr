8b7775b6ef3b541e0fea8aa38d3de0fec9710975
Manuel Selbach <manuel_selbach@yahoo.de>
2021-12-12 18:07:19 +0100

    [TASK] Remove "old" PageTSconfig prior TYPO3 version 9

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,13 +5,7 @@ defined('TYPO3_MODE') or die();
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
         '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extKey . '/Configuration/TypoScript/Extbase/setup.txt">'
     );
-    if (version_compare(TYPO3_branch, '9.5', '>=')) {
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-            '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extKey . '/Configuration/PageTSconfig/Suggest.tsconfig">'
-        );
-    } else {
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-            '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extKey . '/Configuration/PageTSconfig/Suggest_prior_9.tsconfig">'
-        );
-    }
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extKey . '/Configuration/PageTSconfig/Suggest.tsconfig">'
+    );
 })(\Mselbach\StaticInfoTablesFr\Extension::EXTENSION_KEY);
