36ee09ff4aee2ba35cc07e38a97ef43d1108a5c7
Manuel Selbach <manuel_selbach@yahoo.de>
2026-08-13 22:28:56 +0200

    [TASK] Raise version numbers for release

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Static Info Tables (fr)',
     'description' => 'French (fr) language pack for the Static Info Tables providing localized names for countries, currencies and so on.',
     'category' => 'misc',
-    'version' => '13.4.0',
+    'version' => '14.3.0',
     'state' => 'stable',
     'clearcacheonload' => true,
     'author' => 'Manuel Selbach',
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '14.3.0-14.3.99',
-            'static_info_tables' => '13.4.0-13.4.99',
+            'static_info_tables' => '14.3.0-14.3.99',
             'php' => '8.2.0-8.5.99',
         ],
         'conflicts' => [],
